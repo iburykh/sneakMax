49251975d44aeceae45e7c7991c862fc489ba994
Irina Burykh <diva84@mail.ru>
2022-04-05 16:31:42 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import 'nodelist-foreach-polyfill';
+
+// import menuBurger from './modules/menuBurger';
+// import form from './modules/form';
+// import slider from './modules/slider';
+
+'use strict';
+
+// window.noZensmooth = true;
+
+// menuBurger();
