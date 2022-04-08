29c6138018c6311a36357fb7c4b04609788839fb
Irina Burykh <diva84@mail.ru>
2022-04-08 22:54:03 +0300

    catalog

---
+++ src/js/functions/lazy.js
@@ -0,0 +1,35 @@
+const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]');
+const loadMap = document.querySelector('.load-map');
+
+window.addEventListener("scroll", () => {
+	let scrollY = window.scrollY;
+	if (lazyImages.length > 0) {
+		lazyImages.forEach(img => {
+			let imgOffset = img.getBoundingClientRect().top + pageYOffset;
+			
+			if (scrollY >= imgOffset - 1000) {
+				if (img.dataset.src) {
+					img.src = img.dataset.src;
+					img.removeAttribute('data-src');
+				} else if (img.dataset.srcset) {
+					img.srcset = img.dataset.srcset;
+					img.removeAttribute('data-srcset');
+				}
+			}
+		});
+	}
+	//Map
+	// if (!loadMap.classList.contains('loaded')) {
+	// 	let mapOffset = loadMap.offsetTop;
+	// 	if (scrollY >= mapOffset - 200) {
+	// 		const loadMapUrl = loadMap.dataset.map;
+	// 		if (loadMapUrl) {
+	// 			loadMap.insertAdjacentHTML(
+	// 				"beforeend",
+	// 				`<iframe src="${loadMapUrl}" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`
+	// 			);
+	// 			loadMap.classList.add('loaded');
+	// 		}
+	// 	}
+	// }
+});
