5415934c4ad0a227c3965cc45b3295fe59fb63d2
Irina Burykh <diva84@mail.ru>
2022-04-21 23:27:54 +0300

    contacts

---
+++ src/js/functions/map.js
@@ -0,0 +1,29 @@
+let flag = 0;
+
+window.addEventListener('scroll', function(){
+	let scrollY = window.scrollY;
+	let mapOffset = document.querySelector("#map").offsetTop;
+
+	if ((scrollY >= mapOffset - 500) && (flag == 0)) {
+		ymaps.ready(init);
+
+		function init(){
+			const myMap = new ymaps.Map("map", {
+				center: [59.830481, 30.142197],
+				zoom: 10,
+				controls: []
+		
+			});
+			let myPlacemark  = new ymaps.Placemark([59.830481, 30.142197], {}, {
+				iconLayout: 'default#image',
+				iconImageHref: 'img/placemark.png',
+				iconImageSize: [25, 34],
+				iconImageOffset: [-19, -44]
+			});			
+			myMap.geoObjects.add(myPlacemark);
+			myMap.behaviors.disable(['scrollZoom']);
+		}
+
+		flag = 1;
+	}
+});
---
+++ src/js/functions/sizes.js
@@ -0,0 +1,13 @@
+let td = document.querySelectorAll('.catalog-sizes td');
+
+td.forEach(item => {
+	item.addEventListener('click', (e) => {
+		let self = e.currentTarget;
+		item.style.backgroundColor = '#dbbba9';
+		td.forEach(btn => {
+			if (btn !== self) {
+				btn.style.backgroundColor = 'transparent';
+			}
+		});
+	});
+});
